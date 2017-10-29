5a1b187e94bf5cb9997d0f16d3c96b0f0d060b46
techcarepc01 <piash599@gmail.com>
2017-10-29 12:01:28 +0600

    Initial commit

---
+++ loginutil/src/main/java/com/example/loginutil/LoginUtil.java
@@ -0,0 +1,15 @@
+package com.example.loginutil;
+
+import android.util.Log;
+
+/**
+ * Created by Extreme_piash on 10/29/17.
+ */
+
+public class LoginUtil {
+    public static final String TAG = "Log";
+
+    public static void e(String message){
+        Log.e(TAG, message );
+    }
+}
